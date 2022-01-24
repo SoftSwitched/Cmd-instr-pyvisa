8e3800bb0213c9b204874dcc3136cd715237d2cf
SoftSwitched <97022969+SoftSwitched@users.noreply.github.com>
2022-01-24 15:05:53 +0100

    Add files via upload

--- v1.py
+++ v1.py
@@ -6,18 +6,18 @@ Created on Wed Jan  5 15:39:26 2022
 import numpy as np 
 import pyvisa as pv
 import pandas as pd
-
+import time 
 
 
 Instr_lab = pd.DataFrame({'ID' :['MY59201238','MY57252118',' 454132','MY59003642','1420497','MY59002198',' 454121','LCRY3702N14552',
                                  ' 428712','MY59003408','MY57227626','MY59243295','MY59002476','MY59002491',' J00428425',' 1733980001',
-                                 ' 1880310002', ' 2238200001'],
+                                 ' 1880310002', ' 2238200001','CU200081'],
                         'Label':['OSCI015', 'OSCI009','ALIM061','MULTI060','MULTI039','MULTI059','ALIM062','OSCI007','ALIM058',
-                                 'MULTI058','MULTI057','OSCI014','MULTI061','MULTI062','ALIM053','ALIM075','ALIM088', 'ALIM104'],
+                                 'MULTI058','MULTI057','OSCI014','MULTI061','MULTI062','ALIM053','ALIM075','ALIM088', 'ALIM104','GEBF3'],
                         'Brand':['Keysight', 'Keysight','TTi','Keysight','Keythley','Keysight','TTi','Lecroy',
-                                 'TTi','Keysight','Keysight','Keysight','Keysight','Keysight','Sorensen','EA','EA','EA'],
+                                 'TTi','Keysight','Keysight','Keysight','Keysight','Keysight','Sorensen','EA','EA','EA','Tektronix'],
                         'Func' :['Scope','Scope','Supply','Multi','Multi','Multi','Supply','Scope','Supply','Multi',
-                                 'Multi','Scope','Multi','Multi','Supply','Load','Supply', 'Load']})
+                                 'Multi','Scope','Multi','Multi','Supply','Load','Supply', 'Load','GBF']})
 
 
 class Instruments:
@@ -30,6 +30,10 @@ class Instruments:
         self.Func  = Instr_lab.Func[ind]
         self.Adress = Adress
         self.rm = pv.ResourceManager()
+        
+        if(self.Func == 'Scope'): # If scope, bigger buffer
+            self.rm.open_resource(self.Adress).chunk_size = 102400
+
 
         
 ####################### SCOPE ###########################################wv 
@@ -37,13 +41,14 @@ class Instruments:
     def Auto_scale (self):
         if (self.Brand == 'Keysight') and (self.Func == 'Scope'): # Driver Scope Keysight
             self.rm.open_resource(self.Adress).write(':AUToscale')
- 
+
         else : 
             print('Bad call to function or function not implemented')
             
     def Display_chan (self, Channel, ON): # (0 = OFF, 1 = ON)
          if (self.Brand == 'Keysight') and (self.Func == 'Scope'): # Driver Scope Keysight
              self.rm.open_resource(self.Adress).write(':CHANnel'+str(Channel)+':DISPlay '+str(ON))
+
          else : 
              print('Bad call to function or function not implemented')
     
@@ -59,7 +64,9 @@ class Instruments:
              if (Mode_acq ==1):
                  self.rm.open_resource(self.Adress).write(':SINGle')
              else:
-                 self.rm.open_resource(self.Adress).write(':RUN')     
+                 self.rm.open_resource(self.Adress).write(':RUN')   
+             
+
  
          else : 
              print('Bad call to function or function not implemented')   
@@ -69,6 +76,7 @@ class Instruments:
         if (self.Brand == 'Keysight') and (self.Func == 'Scope'): # Driver Scope Keysight
             self.rm.open_resource(self.Adress).write(':TIMebase:RANGe '+str(Scale))
             self.rm.open_resource(self.Adress).write(':TIMebase:POSition '+str(Position))
+
    
         else : 
             print('Bad call to function or function not implemented')
@@ -80,6 +88,7 @@ class Instruments:
            self.rm.open_resource(self.Adress).write('CHANnel'+str(Channel)+':OFFSet '+str(Position)+'V')
            # Value set in the center of the screen
            self.rm.open_resource(self.Adress).write('CHANnel'+str(Channel) +':COUPling '+ Mode)
+
  
        else : 
            print('Bad call to function or function not implemented')   
@@ -89,6 +98,7 @@ class Instruments:
         if (self.Brand == 'Keysight') and (self.Func == 'Scope'): # Driver Scope Keysight
             self.rm.open_resource(self.Adress).write(':CHANnel'+str(Channel)+':LABel ' + '"'+Label+'"')
             self.rm.open_resource(self.Adress).write(':DISPlay:LABel 1')
+
     
         else : 
             print('Bad call to function or function not implemented')
@@ -107,6 +117,7 @@ class Instruments:
                 if(int(self.rm.open_resource(self.Adress).query('CHANnel'+str(i)+':DISPlay?')[0])== 1):
                     self.rm.open_resource(self.Adress).write(':WAVeform:SOURce CHAN'+str(i))
                     data.append(np.asarray(self.rm.open_resource(self.Adress).query('WAVeform:DATA?')[10:].split(",")).astype(float))
+
             return data
         else : 
             print('Bad call to function or function not implemented')
@@ -122,7 +133,8 @@ class Instruments:
             self.rm.open_resource(self.Adress).write('VOLT '+str(Volt)+'V')
             self.rm.open_resource(self.Adress).write('CURR '+str(Amp)+'A')
             self.rm.open_resource(self.Adress).write('POW '+str(Power)+'W')
- 
+
+            
         elif (self.Brand == 'Sorensen') and (self.Func == 'Supply'): #Driver Sorensen supply
             print('Not YET implemented')
              
@@ -138,6 +150,8 @@ class Instruments:
             self.rm.open_resource(self.Adress).write('VOLT:PROT '+str(OVP)+'V')
             self.rm.open_resource(self.Adress).write('CURR:PROT '+str(OCP)+'A')
             self.rm.open_resource(self.Adress).write('POW:PROT '+str(OPP)+'W')
+ 
+            
         elif (self.Brand == 'Sorensen') and (self.Func == 'Supply'): #Driver Sorensen supply
             print('Not YET implemented')
         else : 
@@ -153,6 +167,7 @@ class Instruments:
             volt_prot = float(self.rm.open_resource(self.Adress).query('VOLT:PROT?')[0:-3])
             amp_prot  = float(self.rm.open_resource(self.Adress).query('CURR:PROT?')[0:-3])
             power_prot= float(self.rm.open_resource(self.Adress).query('POW:PROT?')[0:-3])
+
             return volt, amp, power, volt_prot, amp_prot, power_prot
         elif (self.Brand == 'Sorensen') and (self.Func == 'Supply'): #Driver Sorensen supply
             print('Not YET implemented')
@@ -167,6 +182,7 @@ class Instruments:
             self.rm.open_resource(self.Adress).write("*CLS") # Reset config 
             self.rm.open_resource(self.Adress).write("*RST") # Reset config
             self.rm.open_resource(self.Adress).write('OUTP ON')
+
         elif (self.Brand == 'Sorensen') and (self.Func == 'Supply'): #Driver Sorensen supply
             print('Not YET implemented')          
         else : 
@@ -179,6 +195,7 @@ class Instruments:
             self.rm.open_resource(self.Adress).write("*CLS") # Reset config 
             self.rm.open_resource(self.Adress).write("*RST") # Reset config
             self.rm.open_resource(self.Adress).write('OUTP OFF')
+
         elif (self.Brand == 'Sorensen') and (self.Func == 'Supply'): #Driver Sorensen supply
             print('Not YET implemented')          
         else : 
@@ -193,7 +210,8 @@ class Instruments:
             self.rm.open_resource(self.Adress).write("*RST") # Reset config
             self.rm.open_resource(self.Adress).write('VOLT '+str(Volt) +'V')
             self.rm.open_resource(self.Adress).write('CURR '+str(Amp)+'A')
-            self.rm.open_resource(self.Adress).write('POW '+str(Power)+'W')             
+            self.rm.open_resource(self.Adress).write('POW '+str(Power)+'W')    
+
         else : 
             print('Bad call to function or function not implemented')
       
@@ -204,6 +222,7 @@ class Instruments:
             self.rm.open_resource(self.Adress).write('VOLT:PROT '+str(OVP)+'V')
             self.rm.open_resource(self.Adress).write('CURR:PROT '+str(OCP)+'A')
             self.rm.open_resource(self.Adress).write('POW:PROT '+str(OPP)+'W')
+   
         else : 
             print('Bad call to function or function not implemented')
              
@@ -216,6 +235,7 @@ class Instruments:
             volt_prot = float(self.rm.open_resource(self.Adress).query('VOLT:PROT?')[0:-3])
             amp_prot  = float(self.rm.open_resource(self.Adress).query('CURR:PROT?')[0:-3])
             power_prot= float(self.rm.open_resource(self.Adress).query('POW:PROT?')[0:-3])
+
             return volt, amp, power, volt_prot, amp_prot, power_prot             
         else : 
             print('Bad call to function or function not implemented')
@@ -225,6 +245,7 @@ class Instruments:
             self.rm.open_resource(self.Adress).write("*CLS") # Reset config 
             self.rm.open_resource(self.Adress).write("*RST") # Reset config
             self.rm.open_resource(self.Adress).write('INP ON')      
+
         else : 
             print('Bad call to function or function not implemented')
             
@@ -232,7 +253,8 @@ class Instruments:
         if (self.Brand == 'EA') and (self.Func == 'Load'): # Driver  EA load
             self.rm.open_resource(self.Adress).write("*CLS") # Reset config 
             self.rm.open_resource(self.Adress).write("*RST") # Reset config
-            self.rm.open_resource(self.Adress).write('INP OFF')         
+            self.rm.open_resource(self.Adress).write('INP OFF')  
+ 
         else : 
             print('Bad call to function or function not implemented')
             
@@ -240,13 +262,15 @@ class Instruments:
 
     def Mult_set(self, mode_VA, mode_ACDC):
         if (self.Brand == 'Keysight') and (self.Func == 'Multi'): # Driver  Keysight Multi
-            self.rm.open_resource(self.Adress).write('CONF:'+ mode_VA +':'+ mode_ACDC)          
+            self.rm.open_resource(self.Adress).write('CONF:'+ mode_VA +':'+ mode_ACDC)
+
         else : 
             print('Bad call to function or function not implemented')
            
     def Mult_acq(self):
         if (self.Brand == 'Keysight') and (self.Func == 'Multi'): # Driver  Keysight Multi
             data = float(self.rm.open_resource(self.Adress).query('READ?') ) 
+
             return data
         else : 
             print('Bad call to function or function not implemented')
@@ -254,7 +278,38 @@ class Instruments:
             
 ####################### GBF ########################################### 
 
-# Sera créé plus tard ! 
+    def GBF_PWM_set(self, Ton, Period, Amp, dt1 = 100, dt2 = 100): # Duty %, Period us, Amp V, dt1 ns, dt2 ns 
+        if (self.Brand == 'Tektronix') and (self.Func == 'GBF'): # Driver  GBF tektro
+            self.rm.open_resource(self.Adress).write('*CLS')
+            self.rm.open_resource(self.Adress).write('*RST')
+            time.sleep(0.2)
+            # Réglage de la voie 1 
+            self.rm.open_resource(self.Adress).write('SOURce1:FUNCtion PULSe')
+            D1 = ((Ton-(dt2+dt1)/2000)/Period)*100 
+            self.rm.open_resource(self.Adress).write('SOURce1:PULSe:DCYCle '+str(np.floor(D1)))
+            self.rm.open_resource(self.Adress).write('SOURce1:PULSe:PERiod '+str(Period)+'us')
+            self.rm.open_resource(self.Adress).write('SOURce1:PULSe:TRANsition:LEADing MIN')
+            self.rm.open_resource(self.Adress).write('SOURce1:PULSe:TRANsition:TRAiling MIN')
+            self.rm.open_resource(self.Adress).write('SOURce1:PULSe:DELay '+str(dt2)+'ns')
+            self.rm.open_resource(self.Adress).write('SOURce1:VOLTage:LEVel:IMMediate:HIGH ' +str(Amp)+'V')
+            self.rm.open_resource(self.Adress).write('SOURce1:VOLTage:LEVel:IMMediate:LOW 0V') 
+            self.rm.open_resource(self.Adress).write('*SAV 1') 
+            #♦Réglage de la voie 2 
+            D2 = ((Ton+(dt2+dt1)/2000)/Period)*100 
+            self.rm.open_resource(self.Adress).write('SOURce2:FUNCtion PULSe') 
+            self.rm.open_resource(self.Adress).write('SOURce2:PULSe:DCYCle '+str(np.floor(D2)))
+            self.rm.open_resource(self.Adress).write('SOURce2:PULSe:PERiod '+str(Period)+'us')  
+            self.rm.open_resource(self.Adress).write('SOURce2:PULSe:TRANsition:LEADing MIN')
+            self.rm.open_resource(self.Adress).write('SOURce2:PULSe:TRANsition:TRAiling MIN')
+            self.rm.open_resource(self.Adress).write('SOURce2:VOLTage:LEVel:IMMediate:HIGH ' +str(Amp)+'V')
+            self.rm.open_resource(self.Adress).write('SOURce2:VOLTage:LEVel:IMMediate:LOW 0V')
+            self.rm.open_resource(self.Adress).write('OUTPut2:POLarity INV')
+            time.sleep(0.3)
+            self.rm.open_resource(self.Adress).write('*SAV 1') 
+            self.rm.open_resource(self.Adress).write('*RCL 1') 
+   
+        else : 
+            print('Bad call to function or function not implemented')
 
 
 # On scan tous les instruments disponibles pour déterminer ceux qui sont connectés 
